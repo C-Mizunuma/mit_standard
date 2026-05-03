166d4e6023c9fe103937c2d03ca3d5f73f3021f8
Chie Mznm <chitamanya666@gmail.com>
2026-05-04 02:32:52 +0900

    指2.4.1、2.5

---
+++ mit_2.4.1.py
@@ -0,0 +1,2 @@
+birthday = input('Enter your birthday e.g. mm/dd/yyyy: ')
+print('You were born in the year' , birthday(str.substring(str.length() - 4), '.')
---
+++ miy_2.5.py
@@ -0,0 +1,9 @@
+num_x = int(input('How many times should I print the letter X? '))
+to_print = ''
+# ここから下が課題
+i = 0
+while i < num_x:
+    to_print = num_x * 'X'
+    i += 1
+# ここまで
+print(to_print)
